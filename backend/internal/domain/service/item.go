ba68078ffb16487fe9315ee50874af4367ca3348
nickznew1 <nickznew1@gmail.com>
2026-08-13 16:30:39 +0300

    Добавление пакета ответов для endpoints.

--- backend/internal/domain/service/item.go
+++ backend/internal/domain/service/item.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/nickznew1/MagazineMZM/backend/internal/domain/model"
 	"github.com/nickznew1/MagazineMZM/backend/internal/domain/usecase"
+	"github.com/nickznew1/MagazineMZM/backend/pkg/httpRespond"
 	"io"
 	"net/http"
 	"os"
@@ -15,14 +16,16 @@ import (
 
 type ItemService struct {
 	useCase usecase.ItemUseCase
+	respond httpRespond.Respond
 }
 
-func NewItemService(c *usecase.ItemUseCase) *ItemService {
+func NewItemService(c *usecase.ItemUseCase, r httpRespond.Respond) *ItemService {
 	return &ItemService{
+		respond: r,
 		useCase: *c}
 }
 
-func RespondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+/*func RespondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(payload)
@@ -30,21 +33,21 @@ func RespondWithJSON(w http.ResponseWriter, statusCode int, payload interface{})
 
 func RespondWithError(w http.ResponseWriter, statusCode int, message string) {
 	RespondWithJSON(w, statusCode, map[string]string{"error": message})
-}
+}*/
 
 func (h *ItemService) CreateItem(w http.ResponseWriter, r *http.Request) {
 	var input model.Item
 	var documents model.ItemSpecFiles
-	err := r.ParseMultipartForm(10 << 20) // 10 MB
+	err := r.ParseMultipartForm(10 << 20)
 	if err != nil {
-		http.Error(w, "Failed to parse form: "+err.Error(), http.StatusBadRequest)
+		r.RespondWithError(w, http.StatusInternalServerError, "Слишком большой файл")
 		return
 	}
 	Img, imgHandler, err := r.FormFile("imgFile")
 
 	if err != nil {
 		fmt.Println("err: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Ошибка при получении картинки")
+		r.RespondWithError(w, http.StatusInternalServerError, "Ошибка при получении картинки")
 		return
 	}
 	defer Img.Close()
@@ -52,21 +55,21 @@ func (h *ItemService) CreateItem(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		fmt.Println("err: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Ошибка при копировании файла")
+		r.RespondWithError(w, http.StatusInternalServerError, "Ошибка при копировании файла")
 		return
 	}
 	defer imgDestination.Close()
 
 	if _, err = io.Copy(imgDestination, Img); err != nil {
 		fmt.Println("err: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Ошибка при копировании файла")
+		r.RespondWithError(w, http.StatusInternalServerError, "Ошибка при копировании файла")
 		return
 	}
 
 	Pdf, pdfHandler, err := r.FormFile("pdfFile")
 	if err != nil {
 		fmt.Println("err: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Ошибка при получении картинки")
+		r.RespondWithError(w, http.StatusInternalServerError, "Ошибка при получении картинки")
 		return
 	}
 
@@ -74,14 +77,14 @@ func (h *ItemService) CreateItem(w http.ResponseWriter, r *http.Request) {
 	pdfDestination, err := os.Create("./public/documents/" + pdfHandler.Filename)
 	if err != nil {
 		fmt.Println("err: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Ошибка при копировании файла")
+		r.RespondWithError(w, http.StatusInternalServerError, "Ошибка при копировании файла")
 		return
 	}
 	defer pdfDestination.Close()
 
 	if _, err = io.Copy(pdfDestination, Pdf); err != nil {
 		fmt.Println("err: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Ошибка при копировании файла")
+		r.RespondWithError(w, http.StatusInternalServerError, "Ошибка при копировании файла")
 		return
 	}
 
@@ -105,25 +108,25 @@ func (h *ItemService) CreateItem(w http.ResponseWriter, r *http.Request) {
 
 	newItem, _, err := h.useCase.CreateItem(input, documents)
 	if err != nil {
-		RespondWithError(w, http.StatusInternalServerError, "oshibka")
+		r.RespondWithError(w, http.StatusInternalServerError, "oshibka")
 		return
 	}
-	RespondWithJSON(w, http.StatusCreated, newItem)
+	r.RespondWithJSON(w, http.StatusCreated, newItem)
 }
 
 func (h *ItemService) GetItemById(w http.ResponseWriter, r *http.Request) {
 	idStr := chi.URLParam(r, "id")
 	queryItem, err := strconv.Atoi(idStr)
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "cant parse query to int")
+		r.RespondWithError(w, http.StatusBadRequest, "cant parse query to int")
 		return
 	}
 	itemId, err := h.useCase.GetItemById(queryItem)
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "id doesnt find")
+		r.RespondWithError(w, http.StatusBadRequest, "id doesnt find")
 		return
 	}
-	RespondWithJSON(w, http.StatusOK, itemId)
+	r.RespondWithJSON(w, http.StatusOK, itemId)
 
 }
 
@@ -131,80 +134,80 @@ func (h *ItemService) GetItemSpecById(w http.ResponseWriter, r *http.Request) {
 	idStr := chi.URLParam(r, "id")
 	specQuery, err := strconv.Atoi(idStr)
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "cant parse query to int")
+		r.RespondWithError(w, http.StatusBadRequest, "cant parse query to int")
 		return
 	}
 	specId, err := h.useCase.GetSpecById(specQuery)
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "id doesnt find")
+		r.RespondWithError(w, http.StatusBadRequest, "id doesnt find")
 		return
 	}
-	RespondWithJSON(w, http.StatusOK, specId)
+	r.RespondWithJSON(w, http.StatusOK, specId)
 }
 
 func (h *ItemService) GetItemId(w http.ResponseWriter, r *http.Request) {
 	var input model.Item
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		RespondWithError(w, http.StatusBadRequest, "error111")
+		r.RespondWithError(w, http.StatusBadRequest, "error111")
 		return
 	}
 	itemId, err := h.useCase.GetItemId(input)
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "oshibka")
+		r.RespondWithError(w, http.StatusBadRequest, "oshibka")
 		return
 	}
 
-	RespondWithJSON(w, http.StatusOK, itemId)
+	r.RespondWithJSON(w, http.StatusOK, itemId)
 
 }
 
 func (h *ItemService) DeleteItem(w http.ResponseWriter, r *http.Request) {
 	var input model.Item
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		RespondWithError(w, http.StatusBadRequest, "error")
+		r.RespondWithError(w, http.StatusBadRequest, "error")
 		return
 	}
 	deleteItem, err := h.useCase.DeleteItem(input)
 	if err != nil {
-		RespondWithError(w, http.StatusInternalServerError, "oshibka")
+		r.RespondWithError(w, http.StatusInternalServerError, "oshibka")
 		return
 	}
-	RespondWithJSON(w, http.StatusOK, deleteItem)
+	r.RespondWithJSON(w, http.StatusOK, deleteItem)
 }
 
 func (h *ItemService) GetAllItems(w http.ResponseWriter, r *http.Request) {
 	items, err := h.useCase.GetAllItems()
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "error getting all items")
+		r.RespondWithError(w, http.StatusBadRequest, "error getting all items")
 		return
 	}
-	respondWithJSON(w, http.StatusOK, items)
+	r.RespondWithJSON(w, http.StatusOK, items)
 }
 
 func (h *ItemService) ChangeVisible(w http.ResponseWriter, r *http.Request) {
 	var input model.Item
 	idStr := chi.URLParam(r, "id")
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		RespondWithError(w, http.StatusBadRequest, "error")
+		r.RespondWithError(w, http.StatusBadRequest, "error")
 		return
 	}
 	visible, err := h.useCase.ChangeVisible(input.Visible, idStr)
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "oshibka")
+		r.RespondWithError(w, http.StatusBadRequest, "oshibka")
 		return
 	}
 
-	RespondWithJSON(w, http.StatusOK, visible.Visible)
+	r.RespondWithJSON(w, http.StatusOK, visible.Visible)
 
 }
 
 func (h *ItemService) GetAllPropsName(w http.ResponseWriter, r *http.Request) {
 	props, err := h.useCase.GetAllPropsName()
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "error getting all props")
+		r.RespondWithError(w, http.StatusBadRequest, "error getting all props")
 		return
 	}
-	respondWithJSON(w, http.StatusOK, props.PropNameA)
+	r.RespondWithJSON(w, http.StatusOK, props.PropNameA)
 }
 
 func (h *ItemService) SetNewProps(w http.ResponseWriter, r *http.Request) {
@@ -212,14 +215,14 @@ func (h *ItemService) SetNewProps(w http.ResponseWriter, r *http.Request) {
 	idStr := chi.URLParam(r, "id")
 	var input []model.ItemProp
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		RespondWithError(w, http.StatusBadRequest, "error")
+		r.RespondWithError(w, http.StatusBadRequest, "error")
 		return
 	}
 	newProps, err := h.useCase.SetPropsForItem(input, idStr)
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "error")
+		r.RespondWithError(w, http.StatusBadRequest, "error")
 		return
 	}
-	respondWithJSON(w, http.StatusCreated, newProps)
+	r.RespondWithJSON(w, http.StatusCreated, newProps)
 
 }
---
+++ backend/pkg/httpRespond/respond.go
@@ -0,0 +1,16 @@
+package httpRespond
+
+type Respond interface {
+	RespondWithJSON(w http.ResponseWriter, statusCode int, payload interface{})
+	RespondWithError(w http.ResponseWriter, statusCode int, message string)
+}
+
+func RespondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(payload)
+}
+
+func RespondWithError(w http.ResponseWriter, statusCode int, message string) {
+	RespondWithJSON(w, statusCode, map[string]string{"error": message})
+}
